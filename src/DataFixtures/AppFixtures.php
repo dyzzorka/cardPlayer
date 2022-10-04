25941769ae19bbec8e40a68512994ac8c43df6d0
Antoineg3802 <antoine.guerin@ynov.com>
2022-10-04 15:11:29 +0200

    feat:add features

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,40 +5,47 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Card;
+use App\Entity\GameMod;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $output = new ConsoleOutput();
-        $json = file_get_contents(__DIR__.'/cards.json');
-        $json = json_decode($json);
-        $count = 1;
-        $family = ['club', 'diamond', 'heart', 'spade','back'];
-        $family_count = 0;
+        // ANCHOR INSERT ALL CARDS
+        // $output = new ConsoleOutput();
+        // $json = file_get_contents(__DIR__.'/cards.json');
+        // $json = json_decode($json);
+        // $count = 1;
+        // $family = ['club', 'diamond', 'heart', 'spade','back'];
+        // $family_count = 0;
 
-        foreach($json as $key => &$value) {
-            $card = new Card();
-            if ($family[$family_count] == 'back'){
-                $card->setValue(0)
-                    ->setImage($value)
-                    ->setStatus(true)
-                    ->setFamily('back');
-            }else{
-                $card->setValue($count)
-                    ->setImage($value)
-                    ->setStatus(true)
-                    ->setFamily($family[$family_count]);
-            }
-            if ($count==13){
-                $count = 1;
-                $family_count++;
-            }else{
-                $count++;
-            }
-            $manager->persist($card);
-        }
+        // foreach($json as $key => $value) {
+        //     $card = new Card();
+        //     if ($family[$family_count] == 'back'){
+        //         $card->setValue(0)
+        //             ->setImage('img_'. $count . '_'. $family[$family_count])
+        //             ->setStatus(true)
+        //             ->setFamily('back');
+        //     }else{
+        //         $card->setValue($count)
+        //             ->setImage('img_'. $count . '_'. $family[$family_count])
+        //             ->setStatus(true)
+        //             ->setFamily($family[$family_count]);
+        //     }
+        //     if ($count==13){
+        //         $count = 1;
+        //         $family_count++;
+        //     }else{
+        //         $count++;
+        //     }
+        //     $manager->persist($card);
+        // }
+
+        // INSERT ONE GAME TO BDD
+        $game = new GameMod;
+        $game->setName('président')->setDescription('Jeu du président')->setPlayerLimit(4)->setStatus(true);
+        $manager->persist($game);
         $manager->flush();
     }
 }
