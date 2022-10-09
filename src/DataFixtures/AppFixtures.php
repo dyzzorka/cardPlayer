f36041073a81774babe39ecce6be9e1e7983a0b3
dyzzorka <dyzzorka@gmail.com>
2022-10-09 07:27:27 +0200

    access token

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,12 +6,40 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Card;
 use App\Entity\GameMod;
+use App\Entity\User;
 use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * Undocumented variable
+     *
+     * @var UserPasswordHasherInterface
+     */
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
     public function load(ObjectManager $manager): void
     {
+
+
+        for ($i = 0; $i < 10; $i++) {
+            $userUser = new User();
+            $password = "yoyo";
+            $userUser->setUsername("yoyo".$i)->setRoles(['ROLE_USER'])->setPassword($this->passwordHasher->hashPassword($userUser, $password));
+            $manager->persist($userUser);
+        }
+
+        $userUser = new User();
+        $userUser->setUsername("admin")->setRoles(['ROLE_ADMIN'])->setPassword($this->passwordHasher->hashPassword($userUser, "password"));
+        $manager->persist($userUser);
+        $manager->flush();
+
+
         // ANCHOR INSERT ALL CARDS
         // $output = new ConsoleOutput();
         // $json = file_get_contents(__DIR__.'/cards.json');
@@ -43,9 +71,9 @@ class AppFixtures extends Fixture
         // }
 
         // INSERT ONE GAME TO BDD
-        $game = new GameMod;
-        $game->setName('président')->setDescription('Jeu du président')->setPlayerLimit(4)->setStatus(true);
-        $manager->persist($game);
-        $manager->flush();
+        // $game = new GameMod;
+        // $game->setName('président')->setDescription('Jeu du président')->setPlayerLimit(4)->setStatus(true);
+        // $manager->persist($game);
+        // $manager->flush();
     }
 }
